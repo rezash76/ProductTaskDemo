f0727cd570bb83b78d0ffd0b6eb85a3fb0c09295
Reza Sharifi <reza.sharifi76@gmail.com>
2025-04-11 16:52:59 +0330

    Initial Commit

---
+++ InterviewTaskDemo/InterviewTaskDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewTaskDemoApp.swift
+//  InterviewTaskDemo
+//
+//  Created by Reza Sharifi on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct InterviewTaskDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
