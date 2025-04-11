acd7a8b6f5bfb376dab6015a9d354d75cf4929cd
Reza Sharifi <reza.sharifi76@gmail.com>
2025-04-11 17:55:28 +0330

    add Procuct model

--- InterviewTaskDemo/Product.swift
+++ InterviewTaskDemo/Product.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+
+struct ProductAPIResponse: Codable {
+    let products: [Product]
+    let total: Int
+    let skip: Int
+    let limit: Int
+}
+
+struct Product: Codable, Identifiable {
+    let id: Int
+    let title: String
+    let description: String
+    let category: String
+    let price: Double
+    let discountPercentage: Double
+    let rating: Double
+    let stock: Int
+    let tags: [String]
+    let brand: String
+    let sku: String
+    let weight: Double
+    let dimensions: Dimensions
+    let warrantyInformation: String
+    let shippingInformation: String
+    let availabilityStatus: String
+    let reviews: [Review]
+    let returnPolicy: String
+    let minimumOrderQuantity: Int
+    let meta: Meta
+    let thumbnail: String
+    let images: [String]
+}
+
+struct Dimensions: Codable {
+    let width: Double
+    let height: Double
+    let depth: Double
+}
+
+struct Review: Codable {
+    let rating: Int
+    let comment: String
+    let date: String
+    let reviewerName: String
+    let reviewerEmail: String
+}
+
+struct Meta: Codable {
+    let createdAt: String
+    let updatedAt: String
+    let barcode: String
+    let qrCode: String
+}
