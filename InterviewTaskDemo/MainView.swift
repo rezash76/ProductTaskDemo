6a4c4a9e3e782164694dd96d2364d21bfc7f3868
Reza Sharifi <reza.sharifi76@gmail.com>
2025-04-11 17:16:05 +0330

    initial commit

---
+++ InterviewTaskDemo/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  InterviewTaskDemo
+//
+//  Created by Reza Sharifi on 4/11/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            List {
+                Text("asd")
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ InterviewTaskDemo/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  InterviewTaskDemo
+//
+//  Created by Reza Sharifi on 4/11/25.
+//
+
+import Foundation
