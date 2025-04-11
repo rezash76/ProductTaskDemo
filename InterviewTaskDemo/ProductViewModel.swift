096e806601528b714866597b1487ed6e66d307e7
Reza Sharifi <reza.sharifi76@gmail.com>
2025-04-11 18:09:21 +0330

    add ProductViewModel

---
+++ InterviewTaskDemo/ProductViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ProductViewModel.swift
+//  InterviewTaskDemo
+//
+//  Created by Reza Sharifi on 4/11/25.
+//
+
+import SwiftUI
+import Combine
+
+class ProductViewModel: ObservableObject {
+    
+    // MARK: - Published properties
+    @Published var products = [Product]()
+    @Published var isLoading: Bool = false
+    
+    // MARK: - Service
+    private let productService = ProductService()
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        addSubscibers()
+    }
+    
+    private func addSubscibers() {
+        // listen to products data
+        productService.$products
+            .sink { [weak self] products in
+                self?.products = products
+                self?.isLoading = false
+            }
+            .store(in: &cancellables)
+    }
+}
+
